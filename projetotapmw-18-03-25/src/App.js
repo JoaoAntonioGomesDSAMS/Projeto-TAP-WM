683ac085e3f1473dd012cb142debbc1d0c36fa96
JoaoAntonioGomesDSAMS <joaoantoniogms938@gmail.com>
2025-04-08 15:51:34 -0300

    commitando projeto do dia 8 de abril

---
+++ projetotapmw-18-03-25/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Screens/Home';
+import Login from './Screens/Login';
+import Contact from './Screens/Contact';
+import About from './Screens/About';
+import Cadastrar from './Screens/Register'
+import NavBar from './components/Navbar';//ifydtfuygujipkjhgyfdtryughjkl,
+//importante (add pelo prof, serve pra afirma as rotas)
+ 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header className="App-header">
+          <NavBar />
+        </header>
+ 
+        <div className="App-fundo" alt="fundo"></div>
+       
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Cadastrar />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;
---
+++ projetotapmw-18-03-25/src/Screens/Register.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import styles from './Register.module.css';
+ 
+const Register = () => {
+    const [displayName, setDisplayName]=useState('');
+    const [displayEmail, setEmail]  = useState('');
+    const [displayPassword, setPassword]  = useState('');
+    const [displayConfirm, setConfirm]  = useState('');
+    const [error, setError] = useState('');
+    
+    return (
+        <div>
+            <h2>Cadastre-se para ter Acesso ao Site</h2>
+            <form>
+                <label>
+                    <span>
+                        Nome:
+                    </span>
+                    <input type="text" name="displayName" required placeholder="Nome do Usuário" />
+                </label>
+                <label>
+                    <span>
+                        E-mail:
+                    </span>
+                    <input type="text" name="displayName" required placeholder="E-mail" />
+                </label>
+                <label>
+                    <span>
+                        Senha:
+                    </span>
+                    <input type="password" name="displayName" required placeholder="Senha" />
+                </label>
+                <label>
+                    <span>
+                        Confirmar Senha:
+                    </span>
+                    <input type="password" name="displayName" required placeholder="Repetir Senha" />
+                </label>
+                 <button className="btnCadastrar">
+                    Cadastrar
+                </button>
+            </form>
+        </div>
+    )
+}
+ 
+export default Register
