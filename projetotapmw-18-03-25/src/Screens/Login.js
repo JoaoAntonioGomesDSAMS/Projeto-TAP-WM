b020dd145d6d25660477797a0e3ae84e2a2947a4
JoaoAntonioGomesDSAMS <joaoantoniogms938@gmail.com>
2025-04-22 15:45:16 -0300

    ss

--- projetotapmw-18-03-25/src/Screens/Login.js
+++ projetotapmw-18-03-25/src/Screens/Login.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import styles from "./Login.module.css";
 const Login = () => {
+    const [displayName, setDisplayName]=useState('');
+    const [displayEmail, setEmail]  = useState('');
+    const [displayPassword, setPassword]  = useState('');
+    const [error, setError] = useState('');
+    
     return (
-        <div>Logar</div>
+        <div>
+            <h2>Acesso de Login</h2>
+            <form>
+                <label>
+                    <span>
+                        Nome:
+                    </span>
+                    <input type="text" name="displayName" required placeholder="Nome do Usuário" />
+                </label>
+                <label>
+                    <span>
+                        E-mail:
+                    </span>
+                    <input type="text" name="displayName" required placeholder="E-mail" />
+                </label>
+                <label>
+                    <span>
+                        Senha:
+                    </span>
+                    <input type="password" name="displayName" required placeholder="Senha" />
+                </label>
+                 <button className="btnCadastrar">
+                    Login
+                </button>
+            </form>
+        </div>
     )
 }
  
