0f5e5fcee0aeb7ca172d285764fffc93b5c82232
samuel05015 <sh05015130405@gmail.com>
2025-04-29 15:46:14 -0300

    atualizacao 29-04-25 firebase

--- projetotapmw-18-03-25/src/Screens/Login.js
+++ projetotapmw-18-03-25/src/Screens/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
                     <span>
                         E-mail:
                     </span>
-                    <input type="text" name="displayName" required placeholder="E-mail" />
+                    <input type="email" name="displayName" required placeholder="E-mail" />
                 </label>
                 <label>
                     <span>
---
+++ projetotapmw-18-03-25/src/firebase/connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsOF3JWLTh756c2tO9xfR6HHI-6YqhPMY",
+  authDomain: "projeto-tarde-tapwm-4cc74.firebaseapp.com",
+  projectId: "projeto-tarde-tapwm-4cc74",
+  storageBucket: "projeto-tarde-tapwm-4cc74.firebasestorage.app",
+  messagingSenderId: "578873591151",
+  appId: "1:578873591151:web:6059eb83601df8b08d549a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
