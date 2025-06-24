79ccb6af6c58a2ee616673c0a0fe3a9140ef3823
JOAO CESTONARO <joao.cestonaro@etec.sp.gov.br>
2025-06-24 13:51:15 -0300

    asdasdds

--- projetotapmw-18-03-25/src/App.js
+++ projetotapmw-18-03-25/src/App.js
@@ -1,37 +1,39 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './Screens/Home';
 import Login from './Screens/Login';
 import Contact from './Screens/Contact';
 import About from './Screens/About';
-import Cadastrar from './Screens/Register'
-import Product from './Screens/Product'
-import NavBar from './components/Navbar';//ifydtfuygujipkjhgyfdtryughjkl,
-//importante (add pelo prof, serve pra afirma as rotas)
- 
+import Cadastrar from './Screens/Register';
+import Store from './Screens/Store';
+
+import PublicLayout from './components/PublicLayout';
+import Painel from './Screens/Painel';
+import Products from './Screens/Products';
+import Brand from './Screens/Brand';
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <header className="App-header">
-          <NavBar />
-        </header>
- 
-        <div className="App-fundo" alt="fundo"></div>
-       
-        <main className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Cadastrar />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/product" element={<Product/>} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Layout PÚBLICO com navbar e logo */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Cadastrar />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+
+        {/* Layout ADMIN (sem navbar, sem logo) */}
+        <Route path="/painel" element={<Painel />}>
+          <Route path="products" element={<Products />} />
+          <Route path="brand" element={<Brand />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
- 
+
 export default App;
